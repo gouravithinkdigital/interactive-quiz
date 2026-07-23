714e7db28df5f79c671411f957aac51727a31248
gourav <gourav@auvo.io>
2026-07-23 12:50:03 +0530

    fix: added video controls

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -312,6 +312,7 @@ export default function App() {
             onEnded={handleVideoEnd}
             onTimeUpdate={handleTimeUpdate}
             className="w-full"
+            controls
           />
         </div>
 
